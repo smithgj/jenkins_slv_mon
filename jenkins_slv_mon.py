7a7d6ed090600fad124f9f18c934ce9d2f0c98f6
Greg Smith <smithgj66@hotmail.com>
2017-11-09 17:43:37 -0600

    ready to go

--- jenkins_slv_mon.py
+++ jenkins_slv_mon.py
@@ -1,5 +1,41 @@
 import requests
 import sys
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def alert(alert_text):
+    from_addr = "greg.smith@merrillcorp.com"
+    to_addr = "6123211852@tmomail.net"
+
+    # Create message container - the correct MIME type is multipart/alternative.
+    message = MIMEMultipart('alternative')
+    # message['Subject'] = "my subject"
+    message['From'] = from_addr
+    message['To'] = to_addr
+
+    # Create the body of the message
+    html_msg1 = """\
+    <html>
+      <head></head>
+      <body>
+        <p>
+    """
+    html_msg2 = """\
+        </p>
+      </body>
+    </html>
+    """
+    html_msg = html_msg1 + alert_text + html_msg2
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    html_part = MIMEText(html_msg, 'html')
+    message.attach(html_part)
+
+    # Send the message via local SMTP server.
+    server = smtplib.SMTP('relay.stp.mrll.com')
+    server.sendmail(from_addr, to_addr, message.as_string())
+    server.quit()
 
 try:
     target = 'http://us2-jenkins-1.adminsys.mrll.com:8080/computer/svc-us2p-too-12.adminsys.mrll.com/api/json?pretty=true'
@@ -7,10 +43,10 @@ try:
     slv_data.raise_for_status()
 except requests.HTTPError as e:
     print(e)
+    alert('Error connecting to http://us2-jenkins-1.adminsys.mrll.com:8080/computer/svc-us2p-too-7.adminsys.mrll.com/api/json?pretty=true')
+    alert(e)
     sys.exit(1)
-if (not (slv_data.json()).get('offline')) and (not(slv_data.json()).get('temporarilyOffline')):
-    print ('Jenkins slave svc-us2p-too-12.adminsys.mrll.com is up')
-else:
-    print('Jenkins slave svc-us2p-too-12.adminsys.mrll.com is down')
+if ((slv_data.json()).get('offline')) or ((slv_data.json()).get('temporarilyOffline')):
+    print ('Jenkins slave svc-us2p-too-12.adminsys.mrll.com is DOWN')
+    alert('Jenkins slave svc-us2p-too-12.adminsys.mrll.com is DOWN')
 
-input('Hit any key to exit')
